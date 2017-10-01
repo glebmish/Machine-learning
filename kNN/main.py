7876d82a24ecbd89e97f6b75035d84a80057482f
Mishchenko, Gleb <gleb.mishchenko@emc.com>
2017-10-01 16:15:50 +0300

    Implemented kNN classifier

--- kNN/main.py
+++ kNN/main.py
@@ -1,5 +1,21 @@
 
 from kNN.reader import *
+from kNN.KNNClassifier import KNNClassifier
 
 if __name__ == '__main__':
     objects = read_training_set()
+
+    classifier = KNNClassifier(1)
+    classifier.train(objects)
+
+    while True:
+        try:
+            x = float(input('Type x value: '))
+            y = float(input('Type y value: '))
+        except ValueError as e:
+            continue
+
+        point = Object(x, y)
+        point.cls = classifier.classify(point)
+        print(point)
+        print()
