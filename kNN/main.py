04fda3b2d70ceba58820ece0b32eaa0a0a08b428
Mishchenko, Gleb <gleb.mishchenko@emc.com>
2017-10-01 16:35:36 +0300

    Enhanced cli

--- kNN/main.py
+++ kNN/main.py
@@ -3,7 +3,9 @@ from kNN.reader import *
 from kNN.Point import Point
 from kNN.KNNClassifier import KNNClassifier
 
-if __name__ == '__main__':
+def guessing_game():
+    print('Guessing game')
+
     objects = read_training_set()
 
     classifier = KNNClassifier(1)
@@ -14,9 +16,23 @@ if __name__ == '__main__':
             x = float(input('Type x value: '))
             y = float(input('Type y value: '))
         except ValueError as e:
+            print('Wrong value')
             continue
 
         point = Point(x, y)
         point.cls = classifier.classify(point)
         print(point)
         print()
+
+
+if __name__ == '__main__':
+    print("""
+Choose mode:
+    1. Guessing game
+    """)
+
+    type = int(input())
+    if type == 1:
+        guessing_game()
+    else:
+        print('Wrong mode')
