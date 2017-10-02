5e16870257e69cf53cd16434e7d426533b9ae310
Mishchenko, Gleb <gleb.mishchenko@emc.com>
2017-10-02 23:54:13 +0300

    Generalized visualizer

--- kNN/Visualizer.py
+++ kNN/Visualizer.py
@@ -1,35 +1,33 @@
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
+
 
 class Visualizer(object):
 
     def visualize(self, train_set, test_set, classified_set):
-        self.__visualize_circles(train_set)
-        self.__visualize_triangles(classified_set, 15)
-        self.__visualize_triangles(test_set)
+        self.__draw_points(train_set, 'o', 'train')
+        self.__draw_points(classified_set, '^', 'classified', marker_size=15)
+        self.__draw_points(test_set, '^', 'test')
+
         plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.3), ncol=3)
         plt.tight_layout(pad=5)
         self.__show()
 
-    def __visualize_circles(self, point_set):
-        blue_pts = [p for p in point_set if p.cls == 1]
-        blue_x, blue_y = self.__get_x_y(blue_pts)
+    @classmethod
+    def __draw_points(cls, point_set, point_type, set_type, marker_size=8):
+        pts_1 = [p for p in point_set if p.cls == 1]
+        x_1, y_1 = cls.__get_x_y(pts_1)
 
-        red_pts = [p for p in point_set if p.cls == 0]
-        red_x, red_y = self.__get_x_y(red_pts)
+        point_type_1 = 'b{}'.format(point_type)
+        label_1 = '1 type {} set'.format(set_type)
 
-        plt.plot(blue_x, blue_y, 'bo', label='1 type train set')
-        plt.plot(red_x, red_y, 'yo', label='0 type train set')
+        pts_0 = [p for p in point_set if p.cls == 0]
+        x_0, y_0 = cls.__get_x_y(pts_0)
 
-    def __visualize_triangles(self, point_set, marker_size=8):
-        blue_pts = [p for p in point_set if p.cls == 1]
-        blue_x, blue_y = self.__get_x_y(blue_pts)
+        point_type_0 = 'y{}'.format(point_type)
+        label_0 = '0 type {} set'.format(set_type)
 
-        red_pts = [p for p in point_set if p.cls == 0]
-        red_x, red_y = self.__get_x_y(red_pts)
-
-        plt.plot(blue_x, blue_y, 'b^', label='1 type test set', ms=marker_size)
-        plt.plot(red_x, red_y, 'y^', label='0 type test set', ms=marker_size)
+        plt.plot(x_1, y_1, point_type_1, label=label_1, ms=marker_size)
+        plt.plot(x_0, y_0, point_type_0, label=label_0, ms=marker_size)
 
     @staticmethod
     def __get_x_y(point_set):
