9d541053d14f6856b1247d3c9400baa021be9a21
Mishchenko, Gleb <gleb.mishchenko@emc.com>
2017-10-29 16:15:52 +0300

    Created skeleton for regression

--- LinearRegression/main.py
+++ LinearRegression/main.py
@@ -1,12 +1,16 @@
 from LinearRegression.reader import read_training_set
 from LinearRegression.Visualizer import Visualizer
-
+from LinearRegression.regression import LinRegression
 
 def func(x, y):
     return x * y
 
-
 if __name__ == '__main__':
-    points = read_training_set()
+    flats = read_training_set()
+
+    regression = LinRegression()
+    regression.fit(flats)
+    print(regression.predict(flats[0]))
+
     vis = Visualizer()
-    vis.visualize(points, func)
+    vis.visualize(flats, func)
---
+++ LinearRegression/regression.py
@@ -0,0 +1,22 @@
+
+def stupid_fit(X, y):
+    return [1 for x in X]
+
+
+class LinRegression(object):
+
+    def __init__(self, fit_method=stupid_fit):
+        self.W = []
+        self.__fit = fit_method
+
+    def fit(self, train_set):
+        X = [(x.area, x.rooms) for x in train_set]
+        y = [x.price for x in train_set]
+
+        self.W = self.__fit(X, y)
+
+    def predict(self, flat):
+        X = (flat.area, flat.rooms)
+        y = sum([w * x for w, x in zip(self.W, X)])
+
+        return y
