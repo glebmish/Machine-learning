c0bb9d7642d60a3213628dc3e8841153969cf456
= <nosikov-k@mail.ru>
2017-11-13 22:57:24 +0400

    Cross validation

---
+++ Bayes/CrossValidation.py
@@ -0,0 +1,55 @@
+def validate(bayes_class, data, debug=False):
+    # cross validation for 10 buckets
+
+    f_measures = []
+
+    for i in range(10):
+        first_part = data[:9 - i]
+        second_part = data[9 - i + 1:]
+        train_set = first_part + second_part
+        train_set = sum(train_set, [])
+        test_set = data[9 - i]
+
+        bayes = bayes_class()
+        bayes.train(train_set)
+
+        f_measure = count_f(bayes, test_set, debug)
+        f_measures.append(f_measure)
+
+    return f_measures
+
+
+def count_f(bayes, data, debug):
+    # true positive is a spam which is predicted well
+    # true negative is a not spam which is predicted well
+    # false positive is a spam which is not predicted well
+    # false negative is a not spam which is not predicted well
+    # [[ true positive, true negative ],
+    # [ false positive, false negative ]]
+    counts = [[0, 0], [0, 0]]
+
+    for doc in data:
+        if doc.is_spam:
+            if bayes.classify(doc):
+                counts[0][0] += 1
+                type_string = "spam"
+            else:
+                counts[1][0] += 1
+                type_string = "legit"
+        else:
+            if bayes.classify(doc):
+                counts[1][1] += 1
+                type_string = "spam"
+            else:
+                counts[0][1] += 1
+                type_string = "legit"
+
+        if debug: print("{} classified as {}".format(doc.name, type_string))
+
+    if counts[0][0] != 0:
+        recall = counts[0][0] / (counts[0][0] + counts[1][1])
+        precision = counts[0][0] / (counts[0][0] + counts[1][0])
+        f_measure = 2 * precision * recall / (precision + recall)
+    else:
+        f_measure = 0
+    return f_measure
--- Bayes/main.py
+++ Bayes/main.py
@@ -1,28 +1,23 @@
 from Bayes.document import Document
 from Bayes.Bayes_classifier import Bayes
 from Bayes.reader import read_buckets
+from Bayes.CrossValidation import validate
+from Bayes.CrossValidation import count_f
 
 
 if __name__ == "__main__":
     documents_buckets = read_buckets()
+    debug_print = False
+    cross_validation = False
 
-    train_set = sum(documents_buckets[:-2], [])
-    test_set = documents_buckets[-1]
-
-    bayes = Bayes()
-    bayes.train(train_set)
-
-    real_spam = 0
-    predicted_spam = 0
-    for doc in test_set:
-        if doc.is_spam:
-            real_spam += 1
-
-        tpe = bayes.classify(doc)
-        if tpe:
-            predicted_spam += 1
-
-        type_string = "spam" if tpe else "legit"
-        print("{} classified as {}".format(doc.name, type_string))
-
-    print("real: {}, predicted: {}".format(real_spam, predicted_spam))
+    if cross_validation:
+        f_measures = validate(Bayes, documents_buckets, debug_print)
+        for i in range(len(f_measures)):
+            print(i, " - ", f_measures[i])
+    else:
+        train_set = sum(documents_buckets[:-2], [])
+        test_set = documents_buckets[-1]
+        bayes = Bayes()
+        bayes.train(train_set)
+        f_measure = count_f(bayes, test_set, True)
+        print(f_measure)
