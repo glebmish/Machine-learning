f0eb18b8346f6d043e716c438a9748d8a8e7c701
Mishchenko, Gleb <gleb.mishchenko@emc.com>
2017-12-10 23:26:02 +0300

    Changed knn to use shared functionality

---
+++ kNN/kNNClassifier.py
@@ -0,0 +1,40 @@
+from kNN.metrics import Minkovsky
+from kNN.kernels import Gaussian, Sigmoid, Triangular, Epanechnikov, Quartic
+
+import numpy as np
+
+
+class KNNClassifier(object):
+    def __init__(self, k=8, kernel=Gaussian.Gaussian(), metric=Minkovsky.Minkovsky(2)):
+        self.__k = k
+        self.__kernel = kernel
+        self.__metric = metric
+
+        self.__X = []
+        self.__y = []
+
+    def train(self, X, y):
+        self.__X = X
+        self.__y = y
+
+    def predict(self, x):
+        x = x.reshape(2)
+
+        distances = []
+        for sample in self.__X:
+            distances.append(self.__metric.distance(sample, x))
+        closest_distances = sorted(distances)[:self.__k]
+        max_distance = np.array(closest_distances).max()
+
+        # array of indices of points who's distances are included to closest_distances
+        indices = [distances.index(distance) for distance in closest_distances]
+        result = 0
+        for index in indices:
+            label = self.__y[index]
+
+            normalized_distance = distances[index] / max_distance
+            kernel_value = self.__kernel.function(normalized_distance)
+
+            result += kernel_value * label
+
+        return np.sign(result).item()
