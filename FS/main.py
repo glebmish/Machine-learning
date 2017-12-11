7e9c6fd57cddbe07d6fc327c0fa1866cbd6b5842
Nosikov, Konstantin <Konstantin.Nosikov@emc.com>
2017-12-11 08:46:29 +0300

    Created spearman metric. Need to adjust it for normal kNN

---
+++ FS/main.py
@@ -0,0 +1,20 @@
+import os
+from numpy import genfromtxt
+import numpy as np
+from FS.spearman.spearman import *
+
+def main():
+    count_treshold = 0.03
+
+    root = os.path.abspath(os.path.dirname(__file__))
+    TRAIN_X_FILE = os.path.join(root, 'arcene_train.data')
+    TRAIN_Y_FILE = os.path.join(root, 'arcene_train.labels')
+
+    train_X = genfromtxt(TRAIN_X_FILE, delimiter=' ')
+    train_Y = genfromtxt(TRAIN_Y_FILE, delimiter=' ')
+
+    spearman(train_X, train_Y, count_treshold)
+
+
+if __name__ == "__main__":
+    main()
