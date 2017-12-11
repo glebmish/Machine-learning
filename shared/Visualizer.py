3861f8094bff0b3278833eb58f41d337c77b7932
Mishchenko, Gleb <gleb.mishchenko@emc.com>
2017-12-11 22:37:15 +0300

    Better visuzalization (confused points) + real train and test sets

--- shared/Visualizer.py
+++ shared/Visualizer.py
@@ -4,7 +4,7 @@ import matplotlib.cm as cm
 import itertools
 
 
-def plot(X, y, clf, grid_size=50):
+def plot(X, y, X_test, y_guessed, clf, grid_size=50):
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.linspace(x_min, x_max, grid_size),
@@ -26,6 +26,8 @@ def plot(X, y, clf, grid_size=50):
                  alpha=0.8)
     plt.scatter(flatten(X[:, 0]), flatten(X[:, 1]),
                 c=flatten(y), cmap=cm.Paired)
+    plt.scatter(flatten(X_test[:, 0]), flatten(X_test[:, 1]),
+                c=flatten(y_guessed), cmap=cm.Paired, s=4)
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
     plt.show()
