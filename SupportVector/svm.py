22719f0ae2cd451f9dc578f51e9586968ff40a94
Mishchenko, Gleb <gleb.mishchenko@emc.com>
2017-12-12 15:16:04 +0300

    cv

--- SupportVector/svm.py
+++ SupportVector/svm.py
@@ -27,20 +27,13 @@ class SVM(object):
         n_samples, n_features = X.shape
 
         K = self._gram_matrix(X)
-        # Solves
-        # min 1/2 x^T P x + q^T x
-        # s.t.
-        #  Gx \coneleq h
-        #  Ax = b
 
         P = cvxopt.matrix(np.outer(y, y) * K)
         q = cvxopt.matrix(-1 * np.ones(n_samples))
 
-        # -a_i \leq 0
         G_std = cvxopt.matrix(np.diag(np.ones(n_samples) * -1))
         h_std = cvxopt.matrix(np.zeros(n_samples))
 
-        # a_i \leq c
         G_slack = cvxopt.matrix(np.diag(np.ones(n_samples)))
         h_slack = cvxopt.matrix(np.ones(n_samples) * self._c)
 
